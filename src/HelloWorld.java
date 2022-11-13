8892df623f6f6164959358371fec21e4ec3d7a1a
Jacek <jckkupczyk@gmail.com>
2022-11-13 12:55:38 +0100

    Dodanie pętli odwracającej kolejność liter w zdaniu.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,12 @@ public class HelloWorld {
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
+
+        String sentence = "Hello World!!!";
+
+        for (int i = sentence.length() - 1; i >= 0; i--) {
+            System.out.print(sentence.charAt(i));
+        }
     }
+
 }
